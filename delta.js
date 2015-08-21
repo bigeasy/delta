cd314c3b3e75f96a395d0d24ab02c61ce610812d
Alan Gutierrez <alan@prettyrobots.com>
2015-08-20 23:33:10 -0500

    Test error reporting.
    See #6.

--- delta.js
+++ delta.js
@@ -1,69 +1,80 @@
-var rescuers = [], callbacks = []
+var rescuers = [], listeners = []
 
-function Sink (callback, terminator) {
+function Delta (callback) {
     this._callback = callback
-    this._terminator = terminator || noop
     this._waiting = 0
     this._completed = 0
     this._listeners = []
     this._results = []
 }
 
-Sink.prototype.ee = function (ee) {
+Delta.prototype.ee = function (ee) {
     var rescuer = rescuers.pop()
     if (rescuer == null) {
         rescuer = {
-            sink: this,
-            callback: function (error) {
-                rescuer.sink.rescue(error)
+            delta: this,
+            listener: function (error) {
+                rescuer.delta._rescue(error)
                 rescuers.push(rescuer)
             }
         }
     } else {
-        rescuer.sink = this
+        rescuer.delta = this
     }
-    ee.on('error', rescuer)
-    this._eventEmitters.push(ee)
+    ee.on('error', rescuer.listener)
+    this._listeners.push({
+        ee: ee,
+        name: 'error',
+        listener: rescuer.listener,
+        heap: rescuers
+    })
     return new Constructor(this, ee)
 }
 
-Sink.prototype._unlisten = function () {
+Delta.prototype._unlisten = function () {
     for (var i = 0, I = this._listeners.length; i < I; i++) {
         var listener = this._listeners[i]
         listener.ee.removeListener(listener.name, listener.listener)
+        listener.heap.push(listener)
     }
 }
 
-Sink.prototype._complete = function () {
+Delta.prototype._rescue = function (error) {
+    this._unlisten()
+    this._callback.call(null, error)
+}
+
+Delta.prototype._complete = function () {
     if (this._waiting == ++this._complete) {
         this._unlisten()
         var vargs = []
         for (var i = 0, I = this._results.length; i < I; i++) {
             push.apply(vargs, this._results[i])
         }
+        this._callback.apply(null, vargs)
     }
 }
 
-function Constructor (sink, ee) {
-    this._sink = sink
+function Constructor (delta, ee) {
+    this._delta = delta
     this._ee = ee
 }
 
 function gather (vargs) {
-    push.apply(this.sink.results[this.index][0], vargs)
+    push.apply(this._delta._results[this.index][0], vargs)
 }
 
 function get (vargs) {
-    push.apply(this.sink.results[this.index], vargs)
-    this.sink._complete()
+    push.apply(this._delta._results[this.index], vargs)
+    this._delta._complete()
 }
 
 Constructor.prototype.on = function (name, reaction) {
-    var callback = callbacks.pop()
+    var callback = listeners.pop()
 
     if (callback == null) {
         callback = {
-            sink: null,
+            delta: null,
             index: 0,
             action: null,
             listener: function () {
@@ -72,35 +83,37 @@ Constructor.prototype.on = function (name, reaction) {
                     vargs[i] = arguments[i]
                 }
                 callback.listener(vargs)
-                callbacks.push(callback)
             }
         }
     }
 
-    callback.sink = this._sink
+    callback.delta = this._delta
 
     if (Array.isArray(reaction)) {
-        this._sink.results.push([[]])
+        this._delta.results.push([[]])
         callback.action = gather
     } else if (typeof reaction == 'function') {
         callback.action = function (vargs) {
             try {
                 reaction(vargs)
             } catch (error) {
-                this.sink.rescue(error)
+                this._delta._rescue(error)
             }
         }
     } else {
-        this._sink.results.push([])
-        this._sink.waiting++
-        callback.index = this._sink.results.length - 1
+        this._delta.results.push([])
+        this._delta.waiting++
+        callback.index = this._delta.results.length - 1
         callback.action = get
     }
 
-    this._sink._listeners.push({
+    this._delta._listeners.push({
         ee: this._ee,
         name: name,
-        listener: callback.listener
+        listener: callback.listener,
+        heap: listeners
     })
     this._ee.on(name, callback.callback)
 }
+
+module.exports = Delta
--- t/delta/delta.t.js
+++ t/delta/delta.t.js
@@ -1,7 +1,15 @@
 require('proof')(1, prove)
 
 function prove (assert) {
+    var events = require('events')
     var Delta = require('../..')
 
-    assert(Delta, 'require')
+    var delta = new Delta(function (error) {
+        assert(error.message, 'errored', 'errored')
+    })
+
+    var ee = new events.EventEmitter
+    delta.ee(ee)
+
+    ee.emit('error', new Error('errored'))
 }
